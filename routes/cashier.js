a0db5d7d76d81a92206a13eb6418e19b06877c00
yethuaungptu <yethuaung481995@gamil.com>
2019-08-10 21:27:45 +0630

    Ordering process complete

--- routes/cashier.js
+++ routes/cashier.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var Staff = require('../model/Staff');
+var Order = require('../model/Order');
 
 router.get('/login', function(req, res, next) {
     res.render('cashier/login', { title: 'Express' });
   });
-  
+
   router.get('/home', function(req,res,next){
-      res.render('cashier/home')
+      Order.find({status:'3'},{tableNo:1, _id:0},(err,rtn)=>{
+        if(err) throw err;
+        console.log(rtn);
+        res.render('cashier/home',{tables:rtn})
+      })
   });
-  
+
   router.post('/login', function(req,res,next){
     Staff.findOne({$and:[{email:req.body.email},{position:"Cashier"}]},(err,staff)=>{
       if(err) throw err;
@@ -20,5 +25,19 @@ router.get('/login', function(req, res, next) {
       }
     })
   });
+
+  router.post('/ordersigle',(req,res)=>{
+    Order.find({$and:[{status:"3"},{tableNo:req.body.tno}]}).populate('menus.menu_id').populate('drinks.drink_id').exec((err,rtn)=>{
+      if(err) throw err;
+      res.json({order:rtn})
+    })
+  })
+
+  router.get('/checkout/:tno',(req,res)=>{
+    Order.findOneAndUpdate({$and:[{status:"3"},{tableNo:req.params.tno}]},{$set:{status:"4"}},(err,rtn)=>{
+      if(err) throw err;
+      res.redirect('/cashiers/home');
+    })
+  })
 
 module.exports = router;
